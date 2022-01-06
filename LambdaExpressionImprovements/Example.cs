fdb04caaaa2c1affda3654e4f78211db7292d686
Chad Levenhagen <Chad.Levenhagen@polkcountyiowa.gov>
2022-01-05 22:46:05 -0600

    Added two more lambda improvements.

--- LambdaExpressionImprovements/Example.cs
+++ LambdaExpressionImprovements/Example.cs
@@ -11,5 +11,23 @@ public class Example
         var parseIntInferred = (string s) => int.Parse(s);
 
         #endregion
+
+        #region Explicit return types can be declared, when unable to be infered. Previously this wouldn't compile.
+
+        var objectReturnTypeFunction = object (bool boolean) => boolean == true ? 1 : "Hello there!";
+
+        #endregion
+
+        #region Attributes on lambdas
+
+        var withAttributes = ([NumberAttribute(1)] int a, [NumberAttribute(2)] int b) => a + b;
+
+        #endregion
     }
+}
+
+internal class NumberAttribute : Attribute
+{
+    public NumberAttribute(int number)
+    { }
 }
