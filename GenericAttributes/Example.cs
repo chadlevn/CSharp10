57f08696d8a1049329ae108745876ee84c4832ca
Chad Levenhagen <Chad.Levenhagen@polkcountyiowa.gov>
2022-01-05 23:24:41 -0600

    Added generic attributes.

---
+++ GenericAttributes/Example.cs
@@ -0,0 +1,26 @@
+﻿namespace GenericAttributes;
+
+public class TypeAttribute : Attribute
+{
+    public TypeAttribute(Type t) => ParamType = t;
+
+    public Type ParamType { get; }
+}
+
+public class TypeAttributeUsage
+{
+    [TypeAttribute(typeof(string))]
+    public static string? Method() => default;
+}
+
+#region C#10 Preview, need <LangVersion>Preview</LangVersion> in csproj
+
+public class GenericAttribute<T> : Attribute { }
+
+public class GenericAttributeUsage
+{
+    [GenericAttribute<string>()]
+    public static string? Method() => default;
+}
+
+#endregion
---
+++ NullWarnings/Class1.cs
@@ -0,0 +1,32 @@
+﻿namespace NullWarnings
+{
+    public class Class1
+    {
+        public void Method()
+        {
+            var c = new Class1();
+            string representation = "N/A";
+            if ((c != null && c.GetDependentValue(out object obj)) == true)
+            {
+                representation = obj.ToString(); // undesired error
+            }
+
+            // Or, using ?.
+            if (c?.GetDependentValue(out object obj) == true)
+            {
+                representation = obj.ToString(); // undesired error
+            }
+
+            // Or, using ??
+            if (c?.GetDependentValue(out object obj) ?? false)
+            {
+                representation = obj.ToString(); // undesired error
+            }
+        }
+
+        private bool GetDependentValue(out object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
