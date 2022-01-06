a0d50f9ae7b7a5eb47acc85a56abff2bee3c356b
Chad Levenhagen <Chad.Levenhagen@polkcountyiowa.gov>
2022-01-05 22:32:49 -0600

    Added record type sealed to string.

---
+++ RecordTypeSealedToStrings/Program.cs
@@ -0,0 +1,34 @@
+﻿
+Console.WriteLine(new PhysicalContainer("Plastic", 5.0).ToString());
+Console.WriteLine(new WoodContainer(5.0).ToString());
+
+#region C#10 Sealed ToString Console.WriteLine Example
+
+//Console.WriteLine(new SteelSixGallonBucket());
+
+#endregion
+
+// Inline record constructors are a C#9 feature.
+internal record PhysicalContainer(string Material, double Volume)
+{
+    public override string ToString()
+    {
+        return $"A container of {Volume} gallons in size, made entirely of {Material}.";
+    }
+}
+
+internal record WoodContainer(double Volume) : PhysicalContainer("Wood", Volume);
+
+#region C#10 Sealed ToString
+
+internal record SealedContainer(string Material, double Volume)
+{
+    public sealed override string ToString()
+    {
+        return $"A container of {Volume} gallons in size, made entirely of {Material}.";
+    }
+}
+
+internal record SteelSixGallonBucket() : SealedContainer("Steel", 6.0);
+
+#endregion
